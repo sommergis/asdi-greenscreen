8d4361e58e8367e1d3863f612581bee012a4e3ab
Johannes Sommer <js@sommergis.de>
2022-08-06 21:29:10 +0200

    script templates for steps in core processing workflow

---
+++ scripts/02_get_satellite_scenes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# last update: <YYYY-MM-DD>
+# authors: <author>
+
+""" 
+    <template>.py 
+    
+    <Description what this script does>
+
+"""
+
